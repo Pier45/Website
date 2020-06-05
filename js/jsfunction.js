0ce9d9b956bb3864023921fd3443a68855637431
Pier45 <55787954+Pier45@users.noreply.github.com>
2020-06-05 20:34:56 +0200

    v1.9
    -Small changes
    -Fix wrong aspect ratio sponsor
    -Start email js function

--- js/jsfunction.js
+++ js/jsfunction.js
@@ -1,7 +1,12 @@
-
+var storeM = []
 function closeMenu(){
     var eclick = document.getElementById("menu-btn");
-    // eclick.style.maxHeight = "0px";
     eclick.checked = false;
-    console.log(eclick)
+    console.log(eclick.textContent)
+}
+
+function eMail(){
+    const mail = document.getElementById("Email");
+    storeM.push(mail.value)
+    console.log(storeM);
 }
