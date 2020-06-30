e14f02d98d0dd2894066baf795908c0cb6af429d
Pier45 <55787954+Pier45@users.noreply.github.com>
2020-06-30 11:58:54 +0200

    v3.3
    -Small fix in photos

--- js/jsfunction.js
+++ js/jsfunction.js
@@ -1,44 +1,44 @@
-var storeM = []
-const carosS = document.querySelector('.slide');
-const carosI = document.querySelectorAll('.slide img');
+// var storeM = []
+// const carosS = document.querySelector('.slide');
+// const carosI = document.querySelectorAll('.slide img');
 
-const prevB = document.querySelector('#prevB');
-const nextB = document.querySelector('#nextB');
+// const prevB = document.querySelector('#prevB');
+// const nextB = document.querySelector('#nextB');
 
-let counter = 1;
-let sizeImages = carosI[0].clientWidth;
+// let counter = 1;
+// let sizeImages = carosI[0].clientWidth;
 
-nextB.addEventListener('click', function(){
-    if (counter >= carosI.length -1) return;
-    counter++;
-    carosS.style.transition = '0.4s ease-in-out';
-    carosS.style.transform = 'translateX('+ (-sizeImages*counter) + 'px)';
-    console.log(sizeImages, counter);
-})
+// nextB.addEventListener('click', function(){
+//     if (counter >= carosI.length -1) return;
+//     counter++;
+//     carosS.style.transition = '0.4s ease-in-out';
+//     carosS.style.transform = 'translateX('+ (-sizeImages*counter) + 'px)';
+//     console.log(sizeImages, counter);
+// })
 
-prevB.addEventListener('click', function(){
-    if (counter <= 0) return;
-    counter--;
-    carosS.style.transition = '0.4s ease-in-out';
-    carosS.style.transform = 'translateX('+ (-sizeImages*counter) + 'px)';
-    console.log(sizeImages, counter);
-})
+// prevB.addEventListener('click', function(){
+//     if (counter <= 0) return;
+//     counter--;
+//     carosS.style.transition = '0.4s ease-in-out';
+//     carosS.style.transform = 'translateX('+ (-sizeImages*counter) + 'px)';
+//     console.log(sizeImages, counter);
+// })
 
-carosS.addEventListener('transitionend', function(){
-    console.log(carosI[counter].id)
+// carosS.addEventListener('transitionend', function(){
+//     console.log(carosI[counter].id)
 
-    if (carosI[counter].id === 'lastclone'){
-        console.log('sono nella f')
-        carosS.style.transition = 'none';
-        counter = carosI.length - 2;
-        carosS.style.transform = 'translateX('+ (-sizeImages*counter) + 'px)';
-    }
-    if (carosI[counter].id === 'firstclone'){
-        console.log('sono nella f')
-        counter = carosI.length - counter;
-        carosS.style.transform = 'translateX('+ (-sizeImages*counter) + 'px)';
-    }
-})
+//     if (carosI[counter].id === 'lastclone'){
+//         console.log('sono nella f')
+//         carosS.style.transition = 'none';
+//         counter = carosI.length - 2;
+//         carosS.style.transform = 'translateX('+ (-sizeImages*counter) + 'px)';
+//     }
+//     if (carosI[counter].id === 'firstclone'){
+//         console.log('sono nella f')
+//         counter = carosI.length - counter;
+//         carosS.style.transform = 'translateX('+ (-sizeImages*counter) + 'px)';
+//     }
+// })
 
 
 function closeMenu(){
